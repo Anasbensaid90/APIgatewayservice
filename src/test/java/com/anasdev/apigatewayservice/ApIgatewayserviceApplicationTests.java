bf2c1872efc03f361c4ee2d94288a47507780644
Anasbensaid90 <anasbensaid90@gmail.com>
2024-11-11 17:17:50 +0100

    commit

---
+++ src/test/java/com/anasdev/apigatewayservice/ApIgatewayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anasdev.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIgatewayserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
